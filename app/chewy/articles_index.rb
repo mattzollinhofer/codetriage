f0334b024799616c1edd80b2eb9efd5662d55ea8
Matt Zollinhofer <matt.zollinhofer@gmail.com>
2018-11-19 15:20:46 -0500

    add significant_terms support per category

--- app/chewy/articles_index.rb
+++ app/chewy/articles_index.rb
@@ -10,6 +10,43 @@ class ArticlesIndex < Chewy::Index
     field :published_at, type: :date
   end
 
+  def self.sig_categories_for_all_categories
+    aggregations(
+      categories: {
+        terms: { field: "categories" },
+        aggregations: {
+          significant_categories: {
+            significant_terms: {
+              field: "categories"
+            }
+          }
+        }
+      }
+    ).aggs["categories"]["buckets"].each do |category|
+      next if category['key'].include? "Pages"
+      next if category['key'].include? "Archived"
+      next if category['key'].include? "Published"
+      puts
+      puts
+      puts category['key']
+      puts "-"*category['key'].size
+      category["significant_categories"]["buckets"].map{|x| puts "#{x['key']}: #{x['doc_count']}/#{x['bg_count']} -- #{x['score']}" }
+    end;nil
+  end
+
+  def self.sig_categories_for_category(category)
+    exclude = [category, "Published", "Archived"]
+    query(terms: {categories: [category]})
+      .aggregations(significant_categories: {
+        significant_terms: {
+          field: "categories",
+          exclude: exclude
+        }
+      })
+      .aggs["significant_categories"]["buckets"]
+      .map{|x| puts "#{x['key']}: #{x['doc_count']}/#{x['bg_count']} -- #{x['score']}" }
+  end
+
   def self.category(text)
     query( match: {category: text} )
   end
